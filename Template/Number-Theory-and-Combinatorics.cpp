2bf570bc8b66f6ce5e108c4664e620bd66cee9ea
Karry5307 <39618630+Karry5307@users.noreply.github.com>
2018-10-03 20:29:19 +0800

    update

--- Template/Number-Theory-and-Combinatorics.cpp
+++ Template/Number-Theory-and-Combinatorics.cpp
@@ -70,3 +70,74 @@ inline void GaussElimination()
         subsitute(i);
     }
 }
+/*
+By using BSGS,...
+Two fast multiplication templates is provided, the first is based on Divide and Conquer algorithm, the second is based on distributive law.
+*/
+//gg
+map<ll,ll>ht;
+ll mod,val;
+inline ll qmul(ll x,ll y,ll mod)
+{
+    if(!y)
+    {
+        return 0;
+    }
+    ll temp=qmul(x,y>>1,mod);
+    ll res=(temp<<1)%mod;
+    if(y&1)
+    {
+        res=(res+x)%mod;
+    }
+    return res;
+}
+inline ll qmul(ll x,ll y,ll mod)
+{
+    ll l=(y>>25)*x%mod*((1<<25)%mod),r=(y&((1<<25)-1))*x%mod;
+    return (l+r)%mod;
+}
+inline ll qpow(ll base,ll exponent,ll mod)
+{
+    if(!exponent)
+    {
+        return 1;
+    }
+    ll temp=qpow(base,exponent>>1,mod);
+    ll res=qmul(temp,temp,mod);
+    if(exponent&1)
+    {
+        res=qmul(res,base,mod);
+    }
+    return res;
+}
+inline ll BSGS(ll base,ll res,ll mod)
+{
+    ht.clear(),res%=mod;
+    ll temp,val,fail;
+    temp=sqrt((long double)(mod))+1;
+    for(register int i=0;i<temp;i++)
+    {
+        val=qmul(res,qpow(base,i,mod),mod);
+        ht[val]=i;
+    }
+    base=qpow(base,temp,mod);
+    if(!base)
+    {
+        return !res?1:-1;
+    }
+    for(register int i=0;i<=temp;i++)
+    {
+        val=qpow(base,i,mod),fail=ht.find(val)==ht.end()?-1:ht[val];
+        if(fail>=0&&i*temp-fail>=0)
+        {
+            return i*temp-fail;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    val=read(),mod=read();
+    val=(qmul(9,val,mod)+1)%mod;
+    write(BSGS(10,val,mod));
+}
